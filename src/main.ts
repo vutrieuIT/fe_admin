498cc70d74f0531f441bba686334c7abeb7e1cbc
TrieuVD <trieuvd@runsystem.net>
2024-05-20 22:58:11 +0700

    add config primevue, primeflex

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import "/node_modules/primeflex/primeflex.min.css";
+import PrimeVue from "primevue/config";
+
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue);
+app.mount("#app");
