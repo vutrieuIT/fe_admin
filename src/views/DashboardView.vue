745f689b2e3c84315e2bb7cea1dc85a5b8bf9420
TrieuVD <trieuvd@runsystem.net>
2024-06-11 21:50:55 +0700

    update dashbord

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="text-2xl">Dashboard</div>
+  <div class="flex justify-content-center align-items-center">
+    <div class="text-xl ml-2">From Date</div>
+    <Calendar class="ml-2" v-model="fromDate" dateFormat="dd/mm/yy" />
+    <div class="text-xl ml-2">To Date</div>
+    <Calendar class="ml-2" v-model="toDate" dateFormat="dd/mm/yy" />
+    <Button class="ml-2" @click="callSumary">Get Sumary</Button>
+  </div>
   <div class="grid">
     <div class="col-12">
       <h3>order</h3>
@@ -16,16 +23,24 @@
 import { defineComponent, onMounted, ref } from "vue";
 import Chart from "primevue/chart";
 import ApiUtils from "@/util/apiUtil";
+import Calendar from "primevue/calendar";
+import Button from "primevue/button";
 
 export default defineComponent({
   name: "DashboardView",
   components: {
     Chart,
+    Calendar,
+    Button,
   },
   setup() {
     const data = ref({});
 
     const dataRevenue = ref({});
+
+    const toDate = ref(new Date());
+    const fromDate = ref(new Date());
+    fromDate.value.setDate(fromDate.value.getDate() - 10);
 
     const options = {
       aspectRatio: 3,
@@ -39,20 +54,37 @@ export default defineComponent({
     const callApiSales = async () => {
       // call api
       await ApiUtils.get(
-        "/api/sumary/sales?startDate=2024-04-01&endDate=2024-05-01"
-      ).then((res) => {
-        data.value = res.data;
-      });
+        `/api/sumary/sales?startDate=${fromDate.value
+          .toISOString()
+          .slice(0, 10)}&endDate=${toDate.value.toISOString().slice(0, 10)}`
+      )
+        .then((res) => {
+          data.value = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     };
 
     const callApiRevenue = async () => {
       // call api
       await ApiUtils.get(
-        "/api/sumary/revenue?startDate=2024-04-01&endDate=2024-05-01"
-      ).then((res) => {
-        dataRevenue.value = res.data;
-        console.log(dataRevenue.value);
-      });
+        `/api/sumary/revenue?startDate=${fromDate.value
+          .toISOString()
+          .slice(0, 10)}&endDate=${toDate.value.toISOString().slice(0, 10)}`
+      )
+        .then((res) => {
+          dataRevenue.value = res.data;
+          console.log(dataRevenue.value);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    const callSumary = () => {
+      callApiSales();
+      callApiRevenue();
     };
 
     //
@@ -61,7 +93,7 @@ export default defineComponent({
       callApiRevenue();
     });
 
-    return { data, dataRevenue, options };
+    return { data, dataRevenue, options, fromDate, toDate, callSumary };
   },
 });
 </script>
