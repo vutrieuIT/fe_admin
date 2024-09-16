14780e7e4612b74e6911cc69e680fa895dadf667
TrieuVD <trieuvd@runsystem.net>
2024-09-16 22:40:03 +0700

    update dynamic env

--- dockerfile
+++ dockerfile
@@ -4,13 +4,13 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install -f
 COPY . .
-ARG VUE_APP_SERVER_URL
-ENV VUE_APP_SERVER_URL $VUE_APP_SERVER_URL
 RUN npm run build
 
 # Stage 2: Production
 FROM nginx:alpine
 COPY --from=build /app/dist /usr/share/nginx/html
 EXPOSE 80
-ENTRYPOINT [ "entrypoint.sh" ]
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT [ "/entrypoint.sh" ]
 # CMD ["nginx", "-g", "daemon off;"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,12 @@
 
 echo "Starting the entrypoint script"
 
-if [ -z "$API_URL" ]; then
+if [ -z "$API_URL" ] || [-z "$API_PREDICT_URL"]; then
   echo "API_URL is not set. Exiting."
 else
 	echo "API_URL is set to $API_URL"
-	envsubst '$API_URL' < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
-fi
-
-if [ -z "$API_PREDICT_URL" ]; then
-	echo "API_PREDICT_URL is not set. Exiting."
-else
 	echo "API_PREDICT_URL is set to $API_PREDICT_URL"
-	envsubst '$API_PREDICT_URL' < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
+	envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
 fi
 
 echo "Starting nginx"
